c2aaf39943fd4e5fb13efed4b8cee972b53623a4
Shaun Norton <58618146+shaunnnorton@users.noreply.github.com>
2023-01-06 13:30:00 -0800

    Packages hash delete and resize.

---
+++ Models/address.py
@@ -0,0 +1,16 @@
+class Address():
+    ID = None
+    Street = None
+    City = None
+    State = None
+    ZIP = None 
+    connections = []
+    def __init__(self,id: int, street: str, city: str, state: str, zip: str) -> None:
+        self.ID = id
+        self.Street = street
+        self.City = city
+        self.State = state
+        self.ZIP = zip
+
+    def populate_connections(self, connections):
+        pass
--- Models/packages.py
+++ Models/packages.py
@@ -5,18 +5,37 @@ class Packages():
     packages = []
 
     def __init__(self) -> None:
-        for i in range(20):
+        for i in range(self.hash_buckets):
             packages.append([])
 
     def insert_package(self, package: Package):
-        index = Package.ID%20
+        index = Package.ID % self.hash_buckets
         self.packages[index].append(package)
+        if len(self.packages[index]) > self.hash_buckets*1.5:
+            self.resize_hash_table()
 
     def search_packages(self,id: int) -> list:
-        bucket_index = id%20
+        bucket_index = id % self.hash_buckets
         for i in self.packages[bucket_index]:
             if i.ID != id:
                 continue
             else:
                 return i.toList()
         return []
+
+    def delete_package(self, id: int):
+        bucket_index = id % self.hash_buckets
+        for i in self.packages[bucket_index]:
+            if i.ID != id:
+                continue
+            else:
+                self.packages[bucket_index].remove(i)
+
+
+    def resize_hash_table(self):
+        current_table = self.packages
+        self.hash_buckets *= 2
+        self.packages = [[] for i in range(self.hash_buckets)]
+        for bucket in current_table:
+            for item in bucket:
+                self.insert_package(item)
