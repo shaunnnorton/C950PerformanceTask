d22f1b37b6cec6e1c2f6b8678c449b60ce5a5127
Shaun Norton <58618146+shaunnnorton@users.noreply.github.com>
2023-01-06 13:59:27 -0800

    Docstrings and comments. Refactored toList to toTuple. Function to calculate hash bucket from ID.

--- Models/package.py
+++ Models/package.py
@@ -1,5 +1,6 @@
 
 class Package():
+    """Class to provide a data model for a Package"""
     ID = None
     ADDRESS = None
     DEADLINE = None
@@ -8,6 +9,7 @@ class Package():
     ZIP = None
     STATUS = None
     def __init__(self,id: int,address: str,deadline: str,city: str,state: str,zip: str ,status: int) -> None:
+        """Initialize Package with all variables"""
         self.ID = id
         self.ADDRESS = address
         self.DEADLINE = deadline
@@ -16,5 +18,6 @@ class Package():
         self.ZIP = zip
         self.STATUS = status
 
-    def toList(self):
-        return [self.ID, self.ADDRESS, self.DEADLINE, self.CITY, self.STATE, self.ZIP, self.STATUS]
+    def toTuple(self):
+        """Returns a tuple containg all the Package information."""
+        return (self.ID, self.ADDRESS, self.DEADLINE, self.CITY, self.STATE, self.ZIP, self.STATUS)
--- Models/packages.py
+++ Models/packages.py
@@ -1,41 +1,53 @@
 
 from package import Package
 class Packages():
-    hash_buckets = 20
-    packages = []
+    """Class to provide hashtable functionality to sort and store packages."""
+    hash_buckets = 20 #initial hash table size
+    packages = [] #initialize hashtable with 0 buckets
 
     def __init__(self) -> None:
-        for i in range(self.hash_buckets):
+        """Initializes hash table with 20 buckets"""
+        for i in range(self.hash_buckets): #create an empty list 20 times to serve as buckets.
             packages.append([])
 
     def insert_package(self, package: Package):
-        index = Package.ID % self.hash_buckets
-        self.packages[index].append(package)
-        if len(self.packages[index]) > self.hash_buckets*1.5:
-            self.resize_hash_table()
+        """Inserts a Package Object into the hash table."""
+        bucket = self.calculate_bucket(Package.ID)
+        self.packages[bucket].append(package) #Add the Package to the bucket
+        if len(self.packages[bucket]) > self.hash_buckets*1.5: #Check if too many collisions in the table
+            self.resize_hash_table() #Resize the table
 
-    def search_packages(self,id: int) -> list:
-        bucket_index = id % self.hash_buckets
-        for i in self.packages[bucket_index]:
+    def search_packages(self,id: int) -> tuple:
+        """Searches for a package with a matching id in the hash table."""
+        bucket_index = self.calculate_bucket(id) #bucket index of the id
+        for i in self.packages[bucket_index]:#Look through the bucket
             if i.ID != id:
                 continue
             else:
-                return i.toList()
-        return []
+                return i.toTuple() #return a list containing all the information of the package
+        return [] #return an empty list if no Package is found
 
     def delete_package(self, id: int):
-        bucket_index = id % self.hash_buckets
-        for i in self.packages[bucket_index]:
+        """Removes a package from the hash table with a matching ID"""
+        bucket_index = self.calculate_bucket(id) #calculate the bucket to search 
+        for i in self.packages[bucket_index]: #find the package to remove
             if i.ID != id:
                 continue
             else:
-                self.packages[bucket_index].remove(i)
+                self.packages[bucket_index].remove(i) #remove the package from the list if found. 
+
 
 
     def resize_hash_table(self):
-        current_table = self.packages
-        self.hash_buckets *= 2
-        self.packages = [[] for i in range(self.hash_buckets)]
-        for bucket in current_table:
+        """Resizes the hash table to double the amount of buckets and rehashes all items already in the hash table"""
+        current_table = self.packages #store the current hash table
+        self.hash_buckets *= 2 #double the amount of buckets
+        self.packages = [[] for i in range(self.hash_buckets)] #Creates the hash table with the correct number of empty buckets
+        for bucket in current_table: # Repopulates the hashtable with new hashes. 
             for item in bucket:
                 self.insert_package(item)
+
+    def calculate_bucket(id):
+        """Calculatest the bucket in the hash table using an id"""
+        return self.packages % self.hash_buckets # Take the Mod of the id and the amount of buckets. 
+
