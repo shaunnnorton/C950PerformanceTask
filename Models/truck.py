f309d93bf27ed138a286cf9b11357c83ed315212
Shaun Norton <58618146+shaunnnorton@users.noreply.github.com>
2023-01-07 15:07:57 -0800

    Created Truck Model, and added actions to check if package is on the correct truck and if the package i with other required packages.

---
+++ Models/truck.py
@@ -0,0 +1,27 @@
+class Truck():
+    _DRIVER = ''
+    _PACKAGES = {}
+    #_DISTANCE_TRAVELLED = 0
+    _TRUCK_NUMBER = 0
+
+    def setDriver(self, name:str):
+        self._DRIVER = name
+    
+    def getDriver(self):
+        return self._DRIVER
+
+    def addPackage(self, package: int):
+        self._PACKAGES.add(package)
+    
+    def getPackages(self):
+        return self._PACKAGES
+
+    def removePackage(self, package: Package):
+        self._PACKAGES.remove(package)
+
+    def setTruckNumber(self, num: int):
+        self._TRUCK_NUMBER = num
+
+    def getTruckNumber(self):
+        return self._TRUCK_NUMBER
+    
--- Utils/specialNotes.py
+++ Utils/specialNotes.py
@@ -13,28 +13,43 @@ class Actions():
             case specialActions.DELAY:
                 pass
             case specialActions.TRUCK:
-                pass
+                if specialNote[1] != truck.getTruckNumber:
+                    return False
+                else:
+                    return True
             case specialActions.WITH:
-                pass
+                remainingPackages = list(specialNote[2::])
+                truck_packages = truck.getPackages()
+                for package in remainingPackages:
+                    if package in truck_packages:
+                        remainingPackages.delete(package)
+                if remainingPackages == 0:
+                    return True
+                elif len(remainingPackages) + len(truck_packages) <= 16:
+                    for i in remainingPackages:
+                        truck.addPackage(i)
+                    return True
+                else: 
+                    return False
             case specialActions.ADDRESS:
                 pass
 
 
     
     @staticmethod
-    def translateAction(note: str) -> tuple:
+    def translateAction(note: str, package: int) -> tuple:
         split = note.split("|")
         actionString=split[1].strip()
         match actionString[0]:
             case "0":
                 return (0, actionString[1:3],actionString[3:5])
             case "1":
-                return (1, actionString[1::])
+                return (1, int(actionString[1::]))
             case "2":
-                action = [2]
+                action = [2,package]
                 count=1
                 while count<=(len(note)-1):
-                    action.append(actionString[count:count+2])
+                    action.append(int(actionString[count:count+2]))
                     count+=2
                 return tuple(action)
             case "3":
