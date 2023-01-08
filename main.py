47dcfab253a0dc91881968bc26204c676c69fd8a
Shaun Norton <shaunnorton87@gmail.com>
2023-01-07 20:49:18 -0800

    added datetime test

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-import Data.package_data as pd
-import Data.address_data as ad
+from Data.package_data import PackageData
+from Data.address_data import AddressData
 from Utils.packageSelection import selectPackage
-
+from datetime import datetime
 #print(pd.PackageData.getPackages("Data/WGUPS Package File - Sheet1.csv"))
-packages = pd.PackageData.getPackages("Data/WGUPS Package File - Sheet1.csv")
+# packages = pd.PackageData.getPackages("Data/WGUPS Package File - Sheet1.csv")
 
-set_packages = packages.get_packages()
-package40 = packages.search_packages(40)
-#print(package40)
-
-
-addresses = ad.AddressData.getAddresss("Data/DistanceTable - Sheet1.csv")
-print(addresses[26].connections)
+# set_packages = packages.get_packages()
+# package40 = packages.search_packages(40)
+# #print(package40)
 
 
-for i in set_packages:
-    print(selectPackage.selectNextShortest(packages,i,addresses).ID)
+# addresses = ad.AddressData.getAddresss("Data/DistanceTable - Sheet1.csv")
+# print(addresses[26].connections)
+
+
+# for i in set_packages:
+#     print(selectPackage.selectNextShortest(packages,i,addresses).ID)
+
+beginDate = datetime.now().date()
+openingTime = datetime(beginDate.year,beginDate.month, beginDate.day, 8, 0, 0, 0)
+print(beginDate.strftime("%m %d %y %I %M"))
+print(openingTime.strftime("%m %d %y %I %M"))
