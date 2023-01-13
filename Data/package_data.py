69186ded4f350c86d0ed6064daa5be3ac0b12b03
Shaun Norton <58618146+shaunnnorton@users.noreply.github.com>
2023-01-13 07:47:56 -0800

    Package Data Comments

--- Data/package_data.py
+++ Data/package_data.py
@@ -2,6 +2,7 @@ import csv
 from enum import Enum as e
 
 class PackageFields(e):
+    """Provides Standard values and enum for Package fields"""
     #Indexes in CSV
     ID_INDEX = 0
     ADDRESS_INDEX = 1
@@ -19,19 +20,18 @@ class PackageFields(e):
 from Models.packages import Packages
 from Models.package import Package
 
-package_file = "Data/WGUPS Package File - Sheet1.csv"
-
 class PackageData():
-    # package_file = "Data/WGUPS Package File - Sheet1.csv"
+    """Class to parse data from the package csv"""
     @staticmethod
     def getPackages(package_file) -> Packages:
-        new_packages = Packages()
+        """Parses data from the packages CSV to create package objects in a packages hash table."""
+        new_packages = Packages() #Create new packages object. 
         with open(package_file, "r") as pkcsv:
             reader = csv.reader(pkcsv)
-            for i in range(7):
-                reader.__next__()
-            reader.__next__()
-            for row in reader:
+            for i in range(7): #Skip the first seven rows of the CSV as the provide no useful information
+                reader.__next__() 
+            reader.__next__() #skip another row that provides the headers.
+            for row in reader: #For each row create a new package and add it to the packages hash table.
                 new_package = Package(int(row[PackageFields.ID_INDEX.value]),
                                         row[PackageFields.ADDRESS_INDEX.value],
                                         row[PackageFields.DEADLINE_INDEX.value],
