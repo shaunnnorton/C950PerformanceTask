9e6fe4629f55ff3c42e4a7d431baab690597457e
Shaun Norton <58618146+shaunnnorton@users.noreply.github.com>
2023-01-13 10:45:13 -0800

    Documentation for packageSelection, specialNotes, and utils

--- Utils/specialNotes.py
+++ Utils/specialNotes.py
@@ -7,99 +7,97 @@ from Models.address import Address
 from Models.truck import Truck
 
 class specialActions(e):
+    """Enum to provide a standard explination for special actions in the notes"""
     DELAY = 0
     TRUCK = 1
     WITH = 2
     ADDRESS = 3
 
 class Actions():
+    """Provides a class for actions that can be taken from the notes on a package"""
     @staticmethod
-    def verifyAvalible(specialNote: tuple, truck: Truck, currentTime, packages: Packages) -> tuple:
-        if len(specialNote) == 0:
+    def verifyAvalible(specialNote: tuple, truck: Truck, currentTime: timedelta, packages: Packages) -> tuple:
+        """Checks if the conditions of a packages special notes are met and returns a tuple of additionl packages to add."""
+        if len(specialNote) == 0:#If the special note contains no elements default to true with no addtional packages
             return True, ()
-        match specialNote[0]:
-            case specialActions.DELAY.value:
-                # print(specialNote)
-                # print("CASE 1")
-                day,opening = Utils.getDefaultDates()
-                delayedTime = timedelta(hours=specialNote[1],minutes=specialNote[2])
-                if delayedTime < currentTime:
-                    return True , ()
+        match specialNote[0]:#match the first value of the special note to a special action. 
+            case specialActions.DELAY.value: #Action to delay package. 
+                delayedTime = timedelta(hours=specialNote[1],minutes=specialNote[2]) #get the time to delay to
+                if delayedTime < currentTime: #Check if the current time is past the delayed time
+                    return True , () 
                 else:
                     return False , ()
-            case specialActions.TRUCK.value:
-                # print("CASE 2")
-                if specialNote[1] != truck.getTruckNumber():
+            case specialActions.TRUCK.value: #Action to specify a truck for the package
+                if specialNote[1] != truck.getTruckNumber(): #check if the truck number matches that in the special note
                     return False , ()
                 else:
                     return True , ()
-            case specialActions.WITH.value:
-                # print("CASE 3")
-                for group in packages.grouped_packages:
-                    if specialNote[1] in group:
-                        if len(group)+len(truck.getPackages()[-1])<=16:
+            case specialActions.WITH.value: #Action to specify packages to be delivered with this package
+                for group in packages.grouped_packages: #loop the grouped packages set in the packages hash table 
+                    if specialNote[1] in group: #check if the currnent package is in a group
+                        if len(group)+len(truck.getPackages()[-1])<=16: #Check if there is room in the truck for all the grouped packages.
                             return True, tuple(group)
-                        else:
+                        else: #return false if there is not room for the package. 
                             return False, ()
-                return True, ()
-            case specialActions.ADDRESS.value:
-                # print("CASE 4")
-
-                day,opening = Utils.getDefaultDates()
-                delayedTime = timedelta(hours=specialNote[3],minutes=specialNote[4])
-                #print("TEST", currentTime, delayedTime)
-                if delayedTime < currentTime:
-                    package = packages.select_package(specialNote[1])
-                    newAddress = packages.addresses[specialNote[2]]
-                    package.ADDRESS = newAddress.Street
-                    package.ZIP = newAddress.ZIP
+                return True, () #return True if the package is not in a group. 
+            case specialActions.ADDRESS.value: #Action to specify a delay with address change. 
+                delayedTime = timedelta(hours=specialNote[3],minutes=specialNote[4]) #Set the time to delay the packge delivery to
+                if delayedTime < currentTime: #Check it is past the time to delay
+                    package = packages.select_package(specialNote[1]) #Get the package object matching this ID
+                    newAddress = packages.addresses[specialNote[2]] #Get the packages new address
+                    package.ADDRESS = newAddress.Street #set the packages new address
+                    package.ZIP = newAddress.ZIP #set the packages new Zip
                     return True , ()
-                else:
+                else: #return false if the time is too soon. 
                     return False , ()
-            case _:
-                #print("NO CASE")
-
+            case _: #Return True as a default if no case matches
                 return True, ()
 
 
     
     @staticmethod
     def translateAction(note: str, package: int) -> tuple:
-        if note == " | ":
+        """Translates a note from a package into a meaningfull tuple"""
+        if note == " | ": #If the note matches the default case return a default tuple.
             return (-1,package)
-        split = note.split("|")
-        actionString=split[1].strip()
-        match actionString[0]:
-            case "0":
+        split = note.split("|") #Split the note at the bar to gather the second part of the string.
+        actionString=split[1].strip() #Strip the string of whitespaces
+        match actionString[0]: #Match the first character of the string with a case
+            case "0": # Delayed case
+                # Returns format (0, hours, minutes)
                 return (0,int(actionString[1:3]),int(actionString[3:5]))
-            case "1":
+            case "1": # On Truck case
+                # Returns format (0, trucknumber)
                 return (1, int(actionString[1::]))
-            case "2":
+            case "2": # Grouped Case
+                # Returns format (2, packageID, Id1, Id2, ...)
                 action = [2,package]
                 count=1
-                while count+2<=(len(actionString)):
+                while count+2<=(len(actionString)): #add all two digit ids to the return
                     action.append(int(actionString[count:count+2]))
                     count+=2
                 return tuple(action)
-            case "3":
+            case "3": # Address Case
+                # Returns format (3, packageID, newAddressID, delayedHour, delayedMinute)
                 return (3, package ,int(actionString[1:3]),int(actionString[3:5]),int(actionString[5:7]))
-            case _:
+            case _: #Default Case
                 return (-1,package)
 
 
     @staticmethod
     def groupPackages(packages: Packages):
-        for package in packages.get_packages():
-            translatedNote = Actions.translateAction(package.NOTES, package.ID)
-            if (len(translatedNote)!=0) and (translatedNote[0] == 2):
-                if len(packages.grouped_packages) == 0:
+        "Populates the grouped packages of the packages hash table"
+        for package in packages.get_packages():#Iterate through each package
+            translatedNote = Actions.translateAction(package.NOTES, package.ID) #Translate the note on the package
+            if (len(translatedNote)!=0) and (translatedNote[0] == 2): #check the note length > 0 and that it is a grouped package.
+                if len(packages.grouped_packages) == 0: #Create a new set if the grouped packages is empty
                     packages.grouped_packages.append(set(translatedNote[1::]))
-                for index, group in enumerate(packages.grouped_packages):
-                    if any( ele in group for ele in translatedNote[1::]):
+                for index, group in enumerate(packages.grouped_packages): #loop thorugh the grouped packages of the hash table
+                    if any( ele in group for ele in translatedNote[1::]): #If any of the grouped packages are in the set joint the two sets together. 
                         packages.grouped_packages[index] = group.union(set(translatedNote[1::]))
                         break
                     else:
-                        packages.grouped_packages.append(set(translatedNote[1::]))
+                        packages.grouped_packages.append(set(translatedNote[1::])) #Create a new set of gropued packages
             else:
                 continue
     
--- Utils/utils.py
+++ Utils/utils.py
@@ -4,6 +4,7 @@ class Utils():
 
     @staticmethod
     def getDefaultDates()-> tuple:
+        "Return the default Date and opening time."
         todayDT = dt.datetime.today()
         openingTime = dt.timedelta(hours=8)
         return todayDT, openingTime
